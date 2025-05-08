463f1541ec20dcca0cce8755836ef78c3b8b8131
HP <HP@Abhiiii>
2025-05-08 16:15:12 +0530

    Initial Commit

---
+++ src/main/java/in/abhi/Abhi.java
@@ -0,0 +1,19 @@
+package in.abhi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Abhi extends HttpServlet
+{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("abhishek");
+  }
+
+}
